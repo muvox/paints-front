e5c4c4568b8d70b195ac04b0f0c2be4c9363b8ef
Joni Niemi <muvoksi@gmail.com>
2020-02-20 23:33:34 +0200

    fixing layout of paints on the page

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,12 +1,16 @@
-:<template>
-  <v-container fluid>
-    <v-row v-for="paints in this.chunkedPaints" :key="paints.index">
-      <v-col v-for="paint in paints" :key="paint.id">
+<template>
+  <v-container>
+    <v-row
+      v-for="paints in this.chunkedPaints"
+      :key="paints.index"
+      align="start"
+      justify="start"
+    >
+      <v-col cols="3" v-for="paint in paints" :key="paint.id" justify="center">
         <PaintCard :paint="paint" :key="paint.id"></PaintCard>
-        <a>{{ paint.name }} </a>
       </v-col>
     </v-row>
-    <button v-on:click="paskaa">Click</button>
+    <!-- <button v-on:click="paskaa">Click</button> -->
   </v-container>
 </template>
 
--- src/components/common/PaintCard.vue
+++ src/components/common/PaintCard.vue
@@ -1,13 +1,23 @@
 <template>
-  <v-card class="mx-auto" max-width="350" outlined>
-    <span
-      style="
-            height: 100px; width: 100px; border-radius: 50%; display: inline-block;
-            "
+  <v-card class="mx-auto" outlined max-width="350">
+    <!-- <span
+      class="white--text headline"
+      style="height: 100px; width: 100px; border-radius: 50%; display: inline-block;"
       :style="bgColor"
     >
-    </span>
-    <h3>{{ paint.name }}</h3>
+    </span> -->
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">OVERLINE</div>
+        <v-list-item-title class="headline mb-1">
+          {{ paint.name }}</v-list-item-title
+        >
+        <v-list-item-subtitle>
+          Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle
+        >
+      </v-list-item-content>
+      <v-list-item-avatar tile size="80" color="{bgColor}"></v-list-item-avatar>
+    </v-list-item>
   </v-card>
 </template>
 
